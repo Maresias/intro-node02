a87a1a2bd38443e61785c4e49fe0c070b356ea39
Maresias <ale.maresias@gmail.com>
2024-09-25 23:00:25 -0300

    Middleware json

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
-const server = http.createServer(( req, res ) => {
+const server = http.createServer( async ( req, res ) => {
+    //const { method, url } = req
+    await json(req, res)
 
+    return res.writeHead(404).end("Not Found")
 })
 server.listen(3334) 
